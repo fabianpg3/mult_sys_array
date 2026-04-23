1169fbfaa701d7bf5b95a70a250d22d69def8e4c
Jhon <jgaitanaguilar@gmail.com>
2026-04-22 18:40:55 -0600

    Uncomment BIND_STORAGE pragmas for lutram implementation

--- HW/mmult.cpp
+++ HW/mmult.cpp
@@ -151,11 +151,11 @@ void mmult(ap_int<DATA_BIT_SIZE> a[MAX_SIZE * MAX_SIZE], // Read-Only Matrix A
   // Local memory to store input and output matrices with PE-level partitioning
   ap_int<DATA_BIT_SIZE> localA[MAX_SIZE][MAX_SIZE];
 #pragma HLS ARRAY_PARTITION variable = localA dim = 1 factor = PE_ROWS cyclic
-  // #pragma HLS BIND_STORAGE variable = localA type = ram_2p impl = lutram
+#pragma HLS BIND_STORAGE variable = localA type = ram_2p impl = lutram
 
   ap_int<DATA_BIT_SIZE> localB[MAX_SIZE][MAX_SIZE];
 #pragma HLS ARRAY_PARTITION variable = localB dim = 2 factor = PE_COLS cyclic
-  // #pragma HLS BIND_STORAGE variable = localB type = ram_2p impl = lutram
+#pragma HLS BIND_STORAGE variable = localB type = ram_2p impl = lutram
 
   ap_int<2 * DATA_BIT_SIZE> localC[MAX_SIZE][MAX_SIZE];
 #pragma HLS ARRAY_PARTITION variable = localC dim = 1 factor = PE_ROWS cyclic
